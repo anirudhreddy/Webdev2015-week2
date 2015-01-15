138681d74c2e74270b4111616270106fdb4fc666
anirudhreddy <hitchicker18@gmail.com>
2015-01-15 13:10:23 -0500

    updates

---
+++ Comp2068-Webdev2015-week2/Scripts/Game.js
@@ -0,0 +1,23 @@
+﻿var canvas;
+var stage;
+var helloText;
+
+function init() {
+    canvas = document.getElementById("canvas");
+    stage = new createjs.Stage(canvas);
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", gameLoop);
+    main();
+}
+
+function gameLoop() {
+    helloText.x++;
+    helloText.y++;
+    stage.update();
+}
+
+function main() {
+    helloText = new createjs.Text("HelloWorld!", "40px Consolas", "#000000");
+    stage.addChild(helloText);
+}
+//# sourceMappingURL=Game.js.map
---
+++ Comp2068-Webdev2015-week2/Scripts/Game.ts
@@ -0,0 +1,25 @@
+﻿var canvas;
+var stage: createjs.Stage;
+var helloText: createjs.Text;
+
+function init() {
+    canvas = document.getElementById("canvas");
+    stage = new createjs.Stage(canvas);
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", gameLoop);
+    main();
+
+}
+
+function gameLoop() {
+    helloText.x++;
+    helloText.y++;
+    stage.update();
+
+}
+
+function main() {
+     helloText = new createjs.Text("HelloWorld!", "40px Consolas", "#000000");
+    stage.addChild(helloText);
+
+}
